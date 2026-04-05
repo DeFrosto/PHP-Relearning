f45403d32830815ce7c8edc54af35c22a11d2124
DeFrosto <42313221+DeFrosto@users.noreply.github.com>
2026-04-05 00:00:10 -0500

    Create exampleConnection.php

---
+++ login-related/exampleConnection.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('ver')) {
+    http_response_code(403);
+    exit();
+}
+
+$S = "SERVER";
+$U = "USERNAME";
+$P = "PASSWORD";
+$D = "DATABASE";
+
+$conn = null;
+
+try {
+    $conn = new PDO("mysql:host=$S;dbname=$D", $U, $P);
+} catch (PDOException $e) {
+    die("Connection failure! Error: " . $e->getMessage() . "<br/>");
+}
